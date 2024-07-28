34d38dea3a162e116730127ca0baa5c3fe3de75f
shaoyijia <shaoyj.echo@gmail.com>
2024-07-28 23:14:44 +0800

    Support Gemini model.

--- knowledge_storm/lm.py
+++ knowledge_storm/lm.py
@@ -196,6 +196,7 @@ class DeepSeekModel(dspy.OpenAI):
 
 class AzureOpenAIModel(dspy.AzureOpenAI):
     """A wrapper class for dspy.AzureOpenAI."""
+
     def __init__(
             self,
             api_base: Optional[str] = None,
@@ -413,7 +414,7 @@ class OllamaClient(dspy.OllamaLocal):
         super().__init__(model=model, base_url=f"{url}:{port}", **kwargs)
         # Store additional kwargs for the generate method.
         self.kwargs = {**self.kwargs, **kwargs}
-    
+
 
 class TGIClient(dspy.HFClientTGI):
     def __init__(self, model, port, url, http_request_kwargs=None, **kwargs):
@@ -593,3 +594,123 @@ class TogetherClient(dspy.HFModel):
                 completions = [resp_json.get('choices', [])[0].get('text', "")]
             response = {"prompt": prompt, "choices": [{"text": c} for c in completions]}
             return response
+
+
+class GoogleModel(dspy.dsp.modules.lm.LM):
+    """A wrapper class for Google Gemini API."""
+
+    def __init__(
+            self,
+            model: str,
+            api_key: Optional[str] = None,
+            **kwargs,
+    ):
+        """You can use `genai.list_models()` to get a list of available models."""
+        super().__init__(model)
+        try:
+            import google.generativeai as genai
+        except ImportError as err:
+            raise ImportError("GoogleModel requires `pip install google-generativeai`.") from err
+
+        api_key = os.environ.get("GOOGLE_API_KEY") if api_key is None else api_key
+        genai.configure(api_key=api_key)
+
+        kwargs = {
+            "candidate_count": 1,  # Caveat: Gemini API supports only one candidate for now.
+            "temperature": 0.0 if "temperature" not in kwargs else kwargs["temperature"],
+            "max_output_tokens": kwargs["max_tokens"],
+            "top_p": 1,
+            "top_k": 1,
+            **kwargs,
+        }
+
+        kwargs.pop("max_tokens", None)  # GenerationConfig cannot accept max_tokens
+
+        self.model = model
+        self.config = genai.GenerationConfig(**kwargs)
+        self.llm = genai.GenerativeModel(model_name=model,
+                                         generation_config=self.config)
+
+        self.kwargs = {
+            "n": 1,
+            **kwargs,
+        }
+
+        self.history: list[dict[str, Any]] = []
+
+        self._token_usage_lock = threading.Lock()
+        self.prompt_tokens = 0
+        self.completion_tokens = 0
+
+    def log_usage(self, response):
+        """Log the total tokens from the Google API response."""
+        usage_data = response.usage_metadata
+        if usage_data:
+            with self._token_usage_lock:
+                self.prompt_tokens += usage_data.prompt_token_count
+                self.completion_tokens += usage_data.candidates_token_count
+
+    def get_usage_and_reset(self):
+        """Get the total tokens used and reset the token usage."""
+        usage = {
+            self.model:
+                {'prompt_tokens': self.prompt_tokens, 'completion_tokens': self.completion_tokens}
+        }
+        self.prompt_tokens = 0
+        self.completion_tokens = 0
+
+        return usage
+
+    def basic_request(self, prompt: str, **kwargs):
+        raw_kwargs = kwargs
+        kwargs = {
+            **self.kwargs,
+            **kwargs,
+        }
+
+        # Google disallows "n" arguments.
+        n = kwargs.pop("n", None)
+
+        response = self.llm.generate_content(prompt, generation_config=kwargs)
+
+        history = {
+            "prompt": prompt,
+            "response": [response.to_dict()],
+            "kwargs": kwargs,
+            "raw_kwargs": raw_kwargs,
+        }
+        self.history.append(history)
+
+        return response
+
+    @backoff.on_exception(
+        backoff.expo,
+        (Exception,),
+        max_time=1000,
+        max_tries=8,
+        on_backoff=backoff_hdlr,
+        giveup=giveup_hdlr,
+    )
+    def request(self, prompt: str, **kwargs):
+        """Handles retrieval of completions from Google whilst handling API errors"""
+        return self.basic_request(prompt, **kwargs)
+
+    def __call__(
+            self,
+            prompt: str,
+            only_completed: bool = True,
+            return_sorted: bool = False,
+            **kwargs,
+    ):
+        assert only_completed, "for now"
+        assert return_sorted is False, "for now"
+
+        n = kwargs.pop("n", 1)
+
+        completions = []
+        for _ in range(n):
+            response = self.request(prompt, **kwargs)
+            self.log_usage(response)
+            completions.append(response.parts[0].text)
+
+        return completions
