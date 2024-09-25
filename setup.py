0a78efbd81b88f9aadc2825ad0920520bc17cc42
Yucheng-Jiang <51142637+Yucheng-Jiang@users.noreply.github.com>
2024-09-25 00:33:05 -0700

    some dependency requires python>=3.10 modify the requirement in setup.py

--- setup.py
+++ setup.py
@@ -30,10 +30,9 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     install_requires=requirements,
 )
