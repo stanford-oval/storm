854b1a4d7ca9bc6490068bdac33ca55fbb58def9
Eminem <19890913k@163.com>
2024-10-24 09:24:37 +0800

    [SerperRM Bug] valid_url_to_snippets.get(url, {}) get none value.  call strip function will be fix it. just like url.strip("\'")

--- knowledge_storm/rm.py
+++ knowledge_storm/rm.py
@@ -567,7 +567,7 @@ class SerperRM(dspy.Retrieve):
                     snippets = [organic.get("snippet")]
                     if self.ENABLE_EXTRA_SNIPPET_EXTRACTION:
                         snippets.extend(
-                            valid_url_to_snippets.get(url, {}).get("snippets", [])
+                            valid_url_to_snippets.get(url.strip("\'"), {}).get("snippets", [])
                         )
                     collected_results.append(
                         {
