172687311ea6c6ef96d628b75261d00b694f65ed
Iim Nur Diansyah <iimnurdiansyah@gmail.com>
2025-01-20 12:26:27 +0700

    add dockerfile (#4)
    Co-authored-by: iimnurdiansyah <iim.nurdiansyah@ikigai-io.com>

---
+++ frontend/demo_light/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+APP_PID=
+stopRunningProcess() {
+    # Based on https://linuxconfig.org/how-to-propagate-a-signal-to-child-processes-from-a-bash-script
+    if test ! "${APP_PID}" = '' && ps -p ${APP_PID} > /dev/null ; then
+       > /proc/1/fd/1 echo "Stopping ${COMMAND_PATH} which is running with process ID ${APP_PID}"
+
+       kill -TERM ${APP_PID}
+       > /proc/1/fd/1 echo "Waiting for ${COMMAND_PATH} to process SIGTERM signal"
+
+        wait ${APP_PID}
+        > /proc/1/fd/1 echo "All processes have stopped running"
+    else
+        > /proc/1/fd/1 echo "${COMMAND_PATH} was not started when the signal was sent or it has already been stopped"
+    fi
+}
+
+trap stopRunningProcess EXIT TERM
+
+# source ${VIRTUAL_ENV}/bin/activate
+
+streamlit run ${HOME}/app/storm.py &
+APP_ID=${!}
+
+wait ${APP_ID}
